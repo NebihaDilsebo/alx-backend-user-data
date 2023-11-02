27f660beae0c23f591c5dad74759507b38b8f0f1
NebihaDilsebo <shegolenur@hotmail.com>
2023-11-02 09:43:53 -0700

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,3 +13,23 @@ def filter_datum(fields: List[str], redaction: str,
         temp = re.sub(field + "=.*?" + separator,
                       field + "=" + redaction + separator, temp)
     return temp
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class."""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Init class instance."""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filter values in incoming log records."""
+        return filter_datum(
+            self.fields, self.REDACTION, super(
+                RedactingFormatter, self).format(record),
+            self.SEPARATOR)
