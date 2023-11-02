bd4e59f8bb0aca11da27b9a8597f78300dbf1e00
NebihaDilsebo <shegolenur@hotmail.com>
2023-11-02 08:24:04 -0700

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@
 from typing import List
 import re
 
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Return the log message obfuscated."""
